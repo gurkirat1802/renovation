00a90c50e7e9e73accf6c93bca8bc0ba03f8e070
gurkirat1802 <85217914+gurkirat1802@users.noreply.github.com>
2024-11-08 16:21:21 +0530

    Update Card3.jsx

--- src/cards/Card3.jsx
+++ src/cards/Card3.jsx
@@ -2,30 +2,28 @@ import React from "react";
 
 const Card3 = (props) => {
     return (
-        <>
-            <div className="mb-8 border border-gray-300 rounded-lg shadow-lg w-96 bg-white dark:bg-gray-800 hover:cursor-pointer">
-                <figure className="flex flex-col items-center justify-center p-8 text-center bg-white border-b border-gray-300 rounded-t-lg md:rounded-t-none md:rounded-ss-lg md:border-e dark:bg-gray-800 dark:border-gray-700">
-                    <blockquote className="max-w-2xl mx-auto mb-4 text-gray-600 lg:mb-8 dark:text-gray-400">
-                        <h3 className="text-lg font-semibold text-gray-900 dark:text-white">
-                            {props.title}
-                        </h3>
-                        <p className="text-gray-500 dark:text-gray-400">
-                            {props.desc}
-                        </p>
-                    </blockquote>
-                    <figcaption className="flex items-center justify-center">
-                        <div className="text-left rtl:text-right">
-                            <div className="text-lg font-medium dark:text-white">
-                                {props.name}
-                                <span className="ml-1 text-sm text-gray-500 dark:text-gray-400">
-                                    ({props.address})
-                                </span>
-                            </div>
+        <div className="mb-8 border border-gray-300 rounded-lg shadow-lg hover:cursor-pointer h-80 xs:h-64 sm:h-70 med:h-70 lg:h-80 xl:h-60">
+            <figure className="flex flex-col items-center justify-between h-full p-6 text-center bg-white border-b border-gray-300 rounded-lg dark:bg-gray-800 dark:border-gray-700">
+                <blockquote className="w-full text-gray-600 dark:text-gray-400">
+                    <h3 className="xs:text-base sm:text-lg md:text-lg med:text-xl font-semibold text-gray-900 dark:text-white mb-2">
+                        {props.title}
+                    </h3>
+                    <p className="text-gray-500 dark:text-gray-400 text-justify text-xs xs:text-sm sm:text-base">
+                        {props.desc}
+                    </p>
+                </blockquote>
+                <figcaption className="flex items-center justify-center mt-4">
+                    <div className="text-center">
+                        <div className="text-xs xs:text-sm sm:text-base font-medium dark:text-white">
+                            {props.name}
+                            <span className="ml-1 text-xs xs:text-sm sm:text-base text-gray-500 dark:text-gray-400">
+                                ({props.address})
+                            </span>
                         </div>
-                    </figcaption>
-                </figure>
-            </div>
-        </>
+                    </div>
+                </figcaption>
+            </figure>
+        </div>
     );
 }
 
